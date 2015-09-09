3bca51b0a84a1e3437d3926eb561422de34e489f
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-09-08 21:38:26 -0400

    Create factories for Pool and PoolType and use them to add missing relationships

--- app/PoolType.php
+++ app/PoolType.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class PoolType extends Model
 {
+    protected $fillable = ['name', 'rules', 'created_by'];
+
     /**
      * Get the pools for the pool type.
      */
     public function pools()
     {
-        return $this->hasMany('App\Pool');
+        return $this->hasMany('Nhlpool\Pool');
+    }
+
+    /**
+     * Get the user that owns the pool type.
+     */
+    public function user()
+    {
+        return $this->belongsTo('Nhlpool\User', 'created_by');
     }
 }
