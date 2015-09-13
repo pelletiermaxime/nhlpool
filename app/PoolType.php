5c9e54c5df74b7d83effc168c70fcb077e05e276
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-09-13 00:58:36 -0400

    Methods to save and load the rules in a dynamic column. MariaDB magic -_-

--- app/PoolType.php
+++ app/PoolType.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PoolType extends Model
 {
-    protected $fillable = ['name', 'rules', 'created_by'];
+    protected $fillable = ['name', 'created_by'];
 
     /**
      * Get the pools for the pool type.
@@ -22,5 +22,47 @@ class PoolType extends Model
     public function user()
     {
         return $this->belongsTo('Nhlpool\User', 'created_by');
+    }
+
+    /**
+     * Transform a [key => value] array to a [key, value] array
+     * @param  array $array key-value array
+     * @return array key,value array
+     */
+    public static function flatten(array $array) : array
+    {
+        $return = [];
+        array_walk_recursive($array, function($val, $key) use (&$return) {
+            $return[] = $key;
+            $return[] = $val;
+        });
+        return $return;
+    }
+
+    /**
+     * [setRules description]
+     * @param [type] $pool_type_id [description]
+     * @param array  $rules        [description]
+     */
+    public function setRules(array $rules)
+    {
+        $pool_type_id = $this->id;
+        $flatRules = static::flatten($rules);
+        $stringRules = implode("','", $flatRules);
+        // UPDATE pool_types SET rules = COLUMN_CREATE('color', 'blue', 'size', 'XL') WHERE id = 1;
+        \DB::table('pool_types')
+            ->where('id', '=', $pool_type_id)
+            ->update(['rules' => \DB::raw("COLUMN_CREATE('$stringRules')")]);
+    }
+
+    public function getRules()
+    {
+        $pool_type_id = $this->id;
+        // SELECT COLUMN_JSON(rules) FROM pool_types WHERE id = 1;
+        $rules = \DB::table('pool_types')
+             ->select(\DB::raw('COLUMN_JSON(rules) AS rules'))
+             ->where('id', '=', $pool_type_id)
+             ->get();
+        return json_decode($rules[0]->rules);
     }
 }
