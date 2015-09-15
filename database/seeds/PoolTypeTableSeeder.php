2a25bf9242e9eb3503e960d6002804a2dbd06e21
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-09-14 22:58:01 -0400

    Try out a way to map pool types to a Class. Rly not sure if this is gonna work, but doesn't matter :p

--- database/seeds/PoolTypeTableSeeder.php
+++ database/seeds/PoolTypeTableSeeder.php
@@ -1,12 +1,18 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+
+use Nhlpool\PoolTypes\TeamsScoreType;
 
 class PoolTypeTableSeeder extends Seeder
 {
     public function run()
     {
         factory(Nhlpool\PoolType::class, 5)->create();
+        $teamScoreType = new TeamsScoreType();
+        $rulesArray = $teamScoreType->getProperties();
+        $poolType = Nhlpool\PoolType::first();
+        $poolType->setRules($rulesArray);
     }
 }
