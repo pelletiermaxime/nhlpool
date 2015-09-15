c572adddfae0f1e843f4909cbefe1c1a31aa0918
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-09-14 22:58:01 -0400

    Try out a way to map pool types to a Class. Rly not sure if this is gonna work, but doesn't matter :p

---
+++ app/PoolTypes/TeamsScoreType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nhlpool\PoolTypes;
+
+class TeamsScoreType
+{
+    public $properties = [
+        'nbTeams'            => 5,
+        'victoryPoints'      => 5,
+        'lossPoints'         => 0,
+        'goalsForPoints'     => 1,
+        'goalsAgainstPoints' => 0,
+        'shutoutsPoints'     => 5,
+    ];
+
+    public function settings()
+    {
+        return [
+            'nb_teams'             => ['type' => 'range', 'attr' => ['min' => 1, 'max' => 30]],
+            'victory_points'       => ['type' => 'number'],
+            'loss_points'          => ['type' => 'number'],
+            'goals_for_points'     => ['type' => 'number'],
+            'goals_against_points' => ['type' => 'number'],
+            'shutouts_points'      => ['type' => 'number'],
+        ];
+    }
+
+    public function setProperties($property, $value)
+    {
+        $this->properties[$property] = $value;
+    }
+
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+}
